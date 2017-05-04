dd016193e7e4bf3267ad919ad4b863bed08d4aeb
kardos24 <dakardos@hotmail.com>
2017-05-05 01:51:25 +0200

    Mejoras y arreglos en servicios REST

--- src/services/controladorREST.java
+++ src/services/controladorREST.java
@@ -1,19 +1,9 @@
 package services;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
 
 import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -22,29 +12,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.JAXBIntrospector;
-import javax.xml.bind.util.JAXBSource;
-import javax.xml.transform.Result;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 
-import ejercicio3.AnalizadorSAX;
-import ejercicio3.ManejadorSAX;
-import ejercicio3.ProgramaResultado;
-import ejercicio4.AnalizadorDOM;
-import modelo.Favoritos;
-import modelo.ListadoProgramas;
 import servicio.controlador.ServicioALaCarta;
-import servicio.tipos.Emision;
-import servicio.tipos.Programa;
-import servicio.utilidades.Utilidades;
 
 @Path("programas")
 public class controladorREST {
